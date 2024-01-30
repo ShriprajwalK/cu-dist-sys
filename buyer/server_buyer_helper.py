fc71d17820644b4a8f404689cbb436173914ce58
Shriprajwal K <shriprajwalk@gmail.com>
2024-01-30 14:32:01 -0700

    Use dispatch table in helper instead of if/else

--- buyer/server_buyer_helper.py
+++ buyer/server_buyer_helper.py
@@ -1,6 +1,6 @@
 class BuyerServerHelper:
 
-    def __init__(self,customer_db, product_db):
+    def __init__(self, customer_db, product_db):
         self.customer_db = customer_db
         self.product_db = product_db
 
@@ -8,32 +8,39 @@ class BuyerServerHelper:
         response = {"action": action, "type": "buyer"}
 
         response_body = {}
-        if(action=="create_account"):
-            response_body = self.create_account(data)
-        elif(action=="login"):
-            response_body = self.login(data)
-        elif(action=="search"):
-            response_body = self.search(data)
-        elif(action=="cart_add"):
-            response_body = self.cart_add(data)
+
+        action_methods = {
+            "create_account": self.create_account,
+            "login": self.login,
+            "search": self.search,
+            "cart_add": self.cart_add
+        }
+
+        # Get the method based on the action
+        method = action_methods[action]
+
+        if method:
+            response_body = method(data)
+        else:
+            response_body = {"error": f"Unknown action: {action}"}
 
         response["body"] = response_body
 
         return response
 
-    def login(self,data):
+    def login(self, data):
         username = data["body"]["username"]
         password = data["body"]["password"]
         response_body = {}
         try:
-            buyer_id = self.customer_db.check_buyer_credentials(username,password)
+            buyer_id = self.customer_db.check_buyer_credentials(username, password)
 
-            if(buyer_id!=None):
-                response_body = {"login":True,"buyer_id" : buyer_id}
+            if (buyer_id != None):
+                response_body = {"login": True, "buyer_id": buyer_id}
             else:
-                response_body = {"login":False, "error":"Username/Password does not exist"}
+                response_body = {"login": False, "error": "Username/Password does not exist"}
         except Exception as e:
-            response_body = {"login":False, "error":str(e)}
+            response_body = {"login": False, "error": str(e)}
 
         return response_body
 
@@ -43,22 +50,22 @@ class BuyerServerHelper:
         response_body = {}
 
         try:
-            self.customer_db.create_buyer(username,password)
-            response_body= {"is_created":True}
+            self.customer_db.create_buyer(username, password)
+            response_body = {"is_created": True}
         except Exception as e:
-            response_body = {"is_created":False, "error":str(e)}
+            response_body = {"is_created": False, "error": str(e)}
         return response_body
 
-    def jaccard_similarity(self,x,y):
+    def jaccard_similarity(self, x, y):
         intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
         union_cardinality = len(set.union(*[set(x), set(y)]))
-        return intersection_cardinality/float(union_cardinality)
+        return intersection_cardinality / float(union_cardinality)
 
-    def search(self,data):
+    def search(self, data):
         item_keywords = data["body"]["keywords"]
         item_category = data["body"]["item_category"]
         comparing_text = item_keywords.join(item_category)
-        response_body = {"items":[]}
+        response_body = {"items": []}
         similarity_scores = []
         try:
             item_list = self.product_db.get_all_items()
@@ -79,15 +86,15 @@ class BuyerServerHelper:
                 item["price"] = ranked_item[3]
                 item["rating"] = ranked_item[4]
                 response_body["items"].append(item)
-                current_item_number+=1
-                if(current_item_number==num_of_items):
+                current_item_number += 1
+                if (current_item_number == num_of_items):
                     break
 
             return response_body
         except Exception as e:
             return response_body
 
-    def cart_add(self,data):
+    def cart_add(self, data):
         item_id = data["body"]["item_id"]
         requested_quantity = data["body"]["quantity"]
         response_body = {}
@@ -96,21 +103,13 @@ class BuyerServerHelper:
             item = self.product_db.get_item_by_id(item_id)
             available_quantity = item[2]
             price = item[3]
-            if(requested_quantity>=available_quantity):
-                return {"add":False, "Reason":"Requested Quantity Not Present in the Database"}
+            if (requested_quantity >= available_quantity):
+                return {"add": False, "Reason": "Requested Quantity Not Present in the Database"}
 
-            item_details = [item_id, requested_quantity,price]
+            item_details = [item_id, requested_quantity, price]
 
-            response_body = {"add":True, "item_details":item_details}
+            response_body = {"add": True, "item_details": item_details}
             return response_body
         except Exception as e:
             print(e)
-            return {"add":False, "Reason":"Item Not Present in the Database"}
-
-
-
-
-
-
-
-
+            return {"add": False, "Reason": "Item Not Present in the Database"}
--- todo.md
+++ todo.md
@@ -1,6 +1,11 @@
 ## TODO
-[  ] Add Postgres as docker-compose
-[  ] Add buyer server
-[  ] Add seller server
-[  ] Test client-server for buyer
-[  ] Test client-server for seller
+[ x ] Add Postgres as docker-compose
+[ x ] Add buyer server
+[ x ] Test client-server for buyer
+[ ] Change from x import * to named imports
+[ ] Add features for buyer
+[ ] Add seller server
+[ ] Add features for seller
+[ ] Test client-server for seller
+
+
