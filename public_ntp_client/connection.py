80cd77fd481274e7a4ce24671480c589bfbdcbef
Shriprajwal K <shriprajwalk@gmail.com>
2024-03-08 21:54:57 -0700

    Add streaming write to csv

--- public_ntp_client/connection.py
+++ public_ntp_client/connection.py
@@ -1,10 +1,11 @@
 from socket import AF_INET, SOCK_DGRAM
 import socket
 import struct, time
+import pandas as pd
 
 NTPDELTA = 2208988800.0
 buf = 1024
-
+FILENAME = 'public.csv'
 
 def to_ntp_time(t):
     t += NTPDELTA
@@ -37,15 +38,17 @@ class Connection:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.file = open(FILENAME, 'a')
+        # self.file.write('Burst No., Message Pair No., o(i), d(i), theta(i), delta(i), t1, t2, t3, t4\n')
 
     def sendRequest(self, org_ts, rcv_ts):
         xmt_ts = to_ntp_time(time.time())
         a = self.send(org_ts, rcv_ts, xmt_ts)
         ## timeout / message loss
-        while a[2] == -1:
-            print('Retrying:::')
-            xmt_ts = to_ntp_time(time.time())
-            a = self.send(org_ts, rcv_ts, xmt_ts) # to_ntp_time(time.time())
+        # while a[2] == -1:
+        #     print('Retrying:::')
+        #     xmt_ts = to_ntp_time(time.time())
+        #     a = self.send(org_ts, rcv_ts, xmt_ts) # to_ntp_time(time.time())
         return a
 
     def send(self, org_ts, rcv_ts, xmt_ts):
@@ -60,6 +63,7 @@ class Connection:
         try:
             msg, address = client.recvfrom(buf)
         except Exception as e:
+            print("ERROR")
             print(e)
             return (-1, -1, -1, -1)
 
@@ -83,8 +87,8 @@ class Connection:
         # print(unpacked_response)
 
         ## delayesd duplicate
-        if(originate_timestamp < xmt_ts):
-            return (-1,-1,-1,-1)
+        # if(originate_timestamp < xmt_ts):
+        #     return (-1,-1,-1,-1)
 
         # print(struct.unpack("!12I", msg))
         # t1 = time.ctime(struct.unpack( "!12I", msg )[6] - NTPDELTA).replace("  "," ") ### origniate
@@ -104,28 +108,69 @@ class Connection:
 
         delay = calc_delay(t1, t2, t3, t4)
         offset = calc_offset(t1, t2, t3, t4)
-        return delay, offset, originate_timestamp, receive_timestamp
+        return delay, offset, t3, t4, [t1, t2, t3, t4]
 
 
 if __name__ == "__main__":
-
     packet_bursts = 8
     originate_timestamp = 0
     receive_timestamp = 0
-    connection = Connection("127.0.0.1", 82)
-    # connection = Connection("pool.ntp.org", 123)
-    # connection = Connection("35.192.154.114", 1234)
+    #  connection = Connection("127.0.0.1", 82)
+
+    columns = ['Burst No.', 'Message Pair No.', 'o(i)', 'd(i)', 'theta(i)', 'delta(i)']
 
     delay_list = []
     offset_list = []
-    for burst in range(packet_bursts):
-        delay, offset, new_originate_timestamp, new_receive_timestamp = connection.sendRequest(
-            originate_timestamp, receive_timestamp
-        )
-        delay_list.append(delay)
-        offset_list.append(offset)
-        originate_timestamp = new_originate_timestamp
-        receive_timestamp = new_receive_timestamp
+    theta_list = []
+    delta_list = []
+    burst_list = []
+    message_pair_list = []
+    timestamps_list = [[], [], [], []]
+    header = 0
+    for i in range(15):
+        connection = Connection("pool.ntp.org", 123)
+        if header == 0:
+            header += 1
+            connection.file.write('Burst No., Message Pair No., o(i), d(i), theta(i), delta(i), t1, t2, t3, t4\n')
 
-    print(delay_list)
-    print(offset_list)
+        for burst in range(packet_bursts):
+            delay, offset, new_originate_timestamp, new_receive_timestamp, timestamps_list = connection.sendRequest(
+                originate_timestamp, receive_timestamp
+            )
+            theta = 0
+            delta = 0
+            if (i != 0):
+                last_8_delays = delay_list[len(delay_list) - packet_bursts:]
+                min_delay_index = last_8_delays.index(min(last_8_delays)) - packet_bursts
+                theta = offset_list[min_delay_index]
+                theta_list.append(offset_list[min_delay_index])
+                delta = delay_list[min_delay_index]
+                delta_list.append(delay_list[min_delay_index])
+            else:
+                theta_list.append(0)
+                delta_list.append(0)
+            delay_list.append(delay)
+            offset_list.append(offset)
+            burst_list.append(burst + 1)
+            originate_timestamp = new_originate_timestamp
+            receive_timestamp = new_receive_timestamp
+            message_pair_list.append(i + 1)
+            connection.file.write(f'{burst}, {i+1}, {offset}, {delay}, {theta}, {delta}, {timestamps_list[0]}, {timestamps_list[1]}, {timestamps_list[2]}, {timestamps_list[3]}\n')
+        time.sleep(5)
+
+    data = {
+        'Burst No.': burst_list,
+        'Message Pair No.': message_pair_list,
+        'o(i)': offset_list,
+        'd(i)': delay_list,
+        'theta(i)': theta_list,
+        'delta(i)': delta_list,
+        "t1": timestamps_list[0],
+        "t2": timestamps_list[1],
+        "t3": timestamps_list[2],
+        "t4": timestamps_list[3],
+    }
+
+    df = pd.DataFrame(data)
+
+    df.to_csv("data.csv")
